bb2756be2f43d9e5f1da9005e4d4725a30ad5d70
diego7709 <diegotorresi709@gmail.com>
2023-04-19 00:58:16 -0300

    turno agregado

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ let navList = document.querySelector('nav ul');
 menuIcon.addEventListener('click', () => {
   navList.classList.toggle('show');
 });
+/*evento de date*/
+const form = document.getElementById('turno-form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const fechaInput = document.getElementById('fecha');
+  const fecha = new Date(fechaInput.value);
+
+  const hoy = new Date();
+
+  if (fecha < hoy) {
+    alert('La fecha de turno debe ser posterior a la fecha actual.');
+  } else {
+    form.submit();
+  }
+});
